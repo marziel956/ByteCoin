36db90bb14ce42245ac9ae321a44d48ff24b36e1
marziel956 <marziel956@gmail.com>
2024-03-04 12:48:34 +0100

    2

--- ByteCoin/Controller/ViewController.swift
+++ ByteCoin/Controller/ViewController.swift
@@ -41,6 +41,12 @@ class ViewController: UIViewController, UIPickerViewDataSource, UIPickerViewDele
     func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
         return coinManager.currencyArray[row]
     }
-
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row: Int, inComponent component: Int) {
+        let selectedCurrency = coinManager.currencyArray[row]
+        
+        coinManager.getCoinPrice(for: selectedCurrency)
+        
+        print(coinManager.currencyArray[row])
+    }
 }
 
